3909d1b84385f8f6a66a3cf8c888dbebaab70bd0
Guru-1205 <premguru1045@gmail.com>
2025-10-22 21:17:31 +0530

    Refactor formatting and spacing in reverse integer implementation for improved readability

--- 7_ReverseInteger.java
+++ 7_ReverseInteger.java
@@ -1,19 +1,20 @@
 class Solution {
     public int reverse(int x) {
         long sum = 0;
-        while(x!=0){
-            int rem = x%10;
+        while (x != 0) {
+            int rem = x % 10;
             sum += rem;
-           sum = sum*10;
-            x= x/10;
+            sum = sum * 10;
+            x = x / 10;
         }
-        sum = sum/10;
-        if(sum > Integer.MAX_VALUE || sum<Integer.MIN_VALUE){
+        sum = sum / 10;
+        if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
             return 0;
         }
-        if(x<0){
-            return (int)(-1*sum);
+        if (x < 0) {
+            return (int) (-1 * sum);
         }
-        return (int)sum;
+        System.out.println(sum);
+        return (int) sum;
     }
 }
